6d6b07484bcd6a4fbfe855ff4fbb4896a3c07946
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-10 17:07:51 -0500

    correct pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,9 +3,10 @@ from fabric.api import local
 from time import strftime
 from datetime import date
 
+
 def do_pack():
-    """Fabric script that generates a .tgz archive from the contents of the web_static
-    folder of your AirBnB Clone repo"""
+    """Fabric script that generates a .tgz archive from the contents
+    of the web_static folder of your AirBnB Clone repo"""
     file_date = strftime("%Y%m%d%H%M%S")
     file_name = 'web_static_' + file_date + '.tgz'
     try:
