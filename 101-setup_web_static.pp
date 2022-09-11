a056c5689dd46a9d8f8533672709b560d73bce45
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-10 23:17:01 -0500

    correct puppet lint

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,37 +1,46 @@
 # script that sets up your web servers for the deployment of web_static
 
 exec { 'update'
-  command => 'sudo apt-get update',
+  provider => shell,
+  command  => 'sudo apt-get update',
 }
 
 -> exec {'install nginx':
-  command => 'sudo apt-get -y install nginx',
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
 }
 
 -> exec {'test-dir':
-  command => 'sudo mkdir -p /data/web_static/releases/test/',
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
 }
 
 -> exec {'shared-dir':
-  command => 'sudo mkdir -p /data/web_static/shared/',
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/shared/',
 }
 
 -> exec {'index':
-  command => 'sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
+  provider => shell,
+  command  => 'sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
 }
 
 -> exec {'symbolic-link':
-  command => 'sudo ln -sf /data/web_static/releases/test /data/web_static/current',
+  provider => shell,
+  command  => 'sudo ln -sf /data/web_static/releases/test /data/web_static/current',
 }
 
 -> exec {'server-location':
-  command => 'sudo sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+  provider => shell,
+  command  => 'sudo sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
 }
 
 -> exec {'restart':
-  command => 'sudo service nginx restart',
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
 
 -> exec {'grant':
-  command => 'sudo chown -R ubuntu:ubuntu /data',
+  provider => shell,
+  command  => 'sudo chown -R ubuntu:ubuntu /data',
 }
