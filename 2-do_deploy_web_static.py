8fd8b089239d0ef581aeb378ad915735a995b652
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-10 21:29:47 -0500

    correct the mv command - file to filename

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,7 +22,7 @@ def do_deploy(archive_path):
             .format(file, filename))
         run('rm /tmp/{}'.format(file))
         run('mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/'.format(file, filename))
+            /data/web_static/releases/{}/'.format(filename, filename))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(filename))
         run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
