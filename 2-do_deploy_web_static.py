f04cdc7a4cdc133f72a52f3ed75c20f123c4edd2
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-10 22:52:42 -0500

    put comments on some print() - use to debug

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ from os import path
 
 
 env.hosts = ['18.215.143.86', '50.19.37.76']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
@@ -16,10 +17,13 @@ def do_deploy(archive_path):
         # print(f'filename with extension .tgz : {file}')
         filename = file.split('.')[0]
         # print(f'just the filename  : {filename}')
-        put(archive_path, '/tmp/')
+        put(archive_path, "/tmp/")
+        # print("Test put")
         run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        run('tar -zxvf /tmp/{} -C /data/web_static/releases/{}/'
+        # print("Test First")
+        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
             .format(file, filename))
+        # print("Test Second")
         run('rm /tmp/{}'.format(file))
         run('mv /data/web_static/releases/{}/web_static/*\
             /data/web_static/releases/{}/'.format(filename, filename))
@@ -30,4 +34,5 @@ def do_deploy(archive_path):
         print('New version deployed!')
         return True
     except Exception as e:
+        # print(e)
         return False
