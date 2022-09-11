f04b8697ebc848c1c713e2fee8e68d9ae2fe3985
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-10 20:50:40 -0500

    Fabric script that distributes an archive to web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from fabric.api import run, env, put
+from os import path
+
+
+env.hosts = ['18.215.143.86', '50.19.37.76']
+
+
+def do_deploy(archive_path):
+    """Fabric script that distributes an archive to your web servers"""
+    if not path.exists(archive_path):
+        return False
+
+    try:
+        file = archive_path.split('/')[1]
+        # print(f'filename with extension .tgz : {file}')
+        filename = file.split('.')[0]
+        # print(f'just the filename  : {filename}')
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(filename))
+        run('tar -zxvf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file, filename))
+        run('rm /tmp/{}'.format(file))
+        run('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/'.format(file, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(filename))
+    except Exception as e:
+        return False
