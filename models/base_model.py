a459e7db23be4eb5ccf8298a51705060530b759d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-14 00:42:19 -0500

    test

--- models/base_model.py
+++ models/base_model.py
@@ -33,8 +33,6 @@ class BaseModel:
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-
-
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
