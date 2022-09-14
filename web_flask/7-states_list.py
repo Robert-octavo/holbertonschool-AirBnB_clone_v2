abf52b9f5133d908af69d1d7052e19b107f23a0b
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-13 22:24:17 -0500

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
+import models
 from flask import Flask, abort, render_template
-from models import storage
 from models.state import State
 
 
@@ -11,14 +11,14 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_route():
     """Display the states in order”"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+    states = models.storage.all(State)
+    return render_template('7-states_list.html', states=states.values())
 
 
 @app.teardown_appcontext
 def teardown(self):
     """remove the current SQLAlchemy Session"""
-    storage.close()
+    models.storage.close()
 
 
 if __name__ == '__main__':
